aa530e6e24c98cc24c3ff894dab35541be06b254
Sanggil Han <tkdrlf95@naver.com>
2021-07-23 14:13:55 +0900

    feat: start.vue 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <!-- main 으로 시작 -->
-  <Main/>
+  <!-- <Main/> -->
+
+  <!-- start 로 시작 -->
+  <Start/>
 </template>
 
 <script>
-import Main from './views/main/main.vue'
+// import Main from './views/main/main.vue'
+import Start from './views/start/start.vue'
 
 export default {
   name: 'App',
 
   components: {
-    Main,
+    // Main,
+    Start,
   },
 
   data: () => ({
---
+++ frontend/src/views/start/start.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="start-page">
+    <div class="start-background">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.start-page {
+  width: 100%;
+  height: 970px;
+}
+.start-background {
+  height: 100%;
+  background-color: burlywood;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('../../assets/images/startpage.png');
+}
+</style>
