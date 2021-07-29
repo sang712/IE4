b3fbbbba94a63447cae0821a391e9e6a8ba050ac
yjyjk20 <yjyjk20@naver.com>
2021-07-29 14:42:22 +0900

    [BE] fix : 반 정보 조회 api response에 id값 노출되지 않도록 수정

--- backend/src/main/java/com/ssafy/db/entity/EduClass.java
+++ backend/src/main/java/com/ssafy/db/entity/EduClass.java
@@ -1,5 +1,6 @@
 package com.ssafy.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +9,13 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
+@JsonIgnoreProperties({"id"})
 public class EduClass extends BaseEntity{
     int grade;
     int classNo;
     String timetable;
     String classMotto;
     String conferenceUrl;
+
+
 }
