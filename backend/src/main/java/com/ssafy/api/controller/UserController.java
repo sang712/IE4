290438d1e380dde6bd5be874ca8e9641cb6e025f
임아연 <gudwodjssl@gmail.com>
2021-07-29 10:57:31 +0900

    [BE] refactor : USER INFO

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -61,31 +61,26 @@ public class UserController {
 	@ApiResponses({ @ApiResponse(code = 200, message = "성공"), @ApiResponse(code = 401, message = "인증 실패"),
 			@ApiResponse(code = 404, message = "사용자 없음"), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<UserRes> getUserInfo(
-			@ApiIgnore Authentication authentication,
-		   	@RequestParam @ApiParam(value="회원 아이디", required = true) int id,
-			@RequestParam @ApiParam(value="배정 반", required = true) String position) {
+			@ApiIgnore Authentication authentication) {
 		/**
 		 * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
 		 * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
 		 */
 		SsafyUserDetails userDetails = (SsafyUserDetails)authentication.getDetails();
 		String loginId = userDetails.getUsername();
+		User user = userService.getUserByLoginId(loginId);
 
-		if(position == "교사"){
-			User user = userService.getUserByLoginId(loginId);
+		if(user.getPosition() == "교사"){
 			EduClass eduClass = eduClassService.getEduClassByEduClassId(user.getClassId());
 
 			return ResponseEntity.status(200).body(TeacherRes.of(user,eduClass));
 
 		}
 		else{ //(position == "학생")
-			User user = userService.getUserByLoginId(loginId);
 			Student student = userService.getStudentByUserId(user.getId());
 
 			return ResponseEntity.status(200).body(StudentRes.of(user, student));
 		}
-
-
 	}
 
 	@PatchMapping("/{id}")
