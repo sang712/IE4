eea51f7e86a6883854b7a089e79e36e6a7383702
Sanggil Han <tkdrlf95@naver.com>
2021-07-30 16:22:37 +0900

    fix: 시간표가 뜨지 않는 현상 수정

---
+++ frontend/src/views/main/main-section/section-schedule.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="time-table"></div>
+</template>
+
+<script>
+export default {
+  name: 'section-schedule',
+}
+</script>
+
+<style>
+.time-table {
+  background-image: url('../../../assets/images/time-table.png'); 
+}
+</style>
