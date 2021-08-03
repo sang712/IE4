885f9cae87476af4619b3bd956cc48b01a7e5818
kangazy7 <hg_kang7@Naver.com>
2021-08-03 10:31:25 +0900

    [FE] feat: 내정보 보기vue 추가

--- frontend/src/common/lib/vue-router.js
+++ frontend/src/common/lib/vue-router.js
@@ -6,10 +6,7 @@ import SectionHome from '@/views/main/main-section/section-home'
 import SectionSchedule from '@/views/main/main-section/section-schedule'
 import SectionNote from '@/views/main/main-section/section-note'
 import SectionMvp from '@/views/main/main-section/section-mvp'
-import SectionNews from '@/views/main/main-section/section-news'
-import SectionMeterials from '@/views/main/main-section/section-meterials'
-import SectionMyclass from '@/views/main/main-section/section-myclass'
-
+import SectionMypage from '@/views/main/main-section/section-mypage'
 const fullMenu = require('@/views/main/menu.json')
 function makeRoutesFromMenu () {
   let routes = Object.keys(fullMenu).map((key) => { 
@@ -55,30 +52,30 @@ function makeRoutesFromMenu () {
       }
     ]
   })
-// 공지사항버튼 라우터
-  routes.push({
-    path: '/news',
-    name: 'news',
-    component: SectionNews,
-  })
-// 학습자료버튼 라우터
-  routes.push({
-    path: '/meterials',
-    name: 'meterials',
-    component: SectionMeterials,
-  })
-// 우리반버튼 라우터
-  routes.push({
-    path: '/myclass',
-    name: 'myclass',
-    component: SectionMyclass,
-  })
-// // 내정보버튼 라우터
+// // 공지사항버튼 라우터
 //   routes.push({
-//     path: '/mypage',
-//     name: 'mypage',
-//     component: SectionMypage,
+//     path: '/news',
+//     name: 'news',
+//     component: SectionNews,
 //   })
+// // 학습자료버튼 라우터
+//   routes.push({
+//     path: '/meterials',
+//     name: 'meterials',
+//     component: SectionMeterials,
+//   })
+// // 우리반버튼 라우터
+//   routes.push({
+//     path: '/myclass',
+//     name: 'myclass',
+//     component: SectionMyclass,
+//   })
+// 내정보버튼 라우터
+  routes.push({
+    path: '/mypage',
+    name: 'mypage',
+    component: SectionMypage,
+  })
   return routes
 }
 // const router = new VueRouter({
---
+++ frontend/src/views/main/main-section/section-mypage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mypage-out-background d-flex justify-content-center row">
+    <div class="mypage-in-background ">
+      <div class="d-flex flex-row">
+        <div style="margin-right:50px">
+          <div class="profile-img" style="width: 230px ; width: 230px; margin-left: 50px"></div>
+          <el-button style="width: 60%; height: 10%; font-size: 120% ;margin-left: 82px; ">프로필수정</el-button>     
+        </div>
+        <div class="mypage-second-background">
+
+<!-- ###### 여기에다가 회원정보값을 넣으면 됩니다.###### -->
+          <h2>아이디:</h2>
+          <h3>dddddddddd</h3>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-evenly">
+      <el-button style="width: 13%; height: 70%; font-size: 120%">수정</el-button> 
+      <el-button style="width: 13%; height: 70%; font-size: 120%">탈퇴</el-button> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .mypage-out-background {
+    background-color: #efeee9 ;
+    margin: 5px 5px;
+    height: 66vh;
+    width: 83vw;
+  }
+  .mypage-in-background {
+    background-color: #ffffff ;
+    height: 52vh;
+    width: 80vw;
+    margin: 15px 5px 0px 5px;
+  }
+  .mypage-second-background {
+    background-color: #efeee9 ;
+    margin-top: 30px;
+    margin-right: 60px;
+    height: 430px;
+    width: 55vw;
+  }
+</style>
