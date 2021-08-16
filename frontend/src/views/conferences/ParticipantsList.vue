4c92f9d1631d179a4b03e903acb96da068895154
yjyjk20 <yjyjk20@naver.com>
2021-08-17 03:32:10 +0900

    [FE] fix : 일단 내가 알기 쉬운 코드로 변환 ㅜ

--- frontend/src/views/conferences/ParticipantsList.vue
+++ frontend/src/views/conferences/ParticipantsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="participants-list" class="col-3" style="display: none;">
     <div id="participants-title">참석자 명단</div>
-    <button class="button" type="button" @click="getParticipant">참석자 가져오기</button>
+    <button class="button" type="button" @click="getParticipant()">참석자 가져오기</button>
     <div id="participants-views"></div>
   </div>
 </template>
@@ -12,35 +12,16 @@ import * as conference from '../main/conference.js'
 
 export default {
   name: 'ParticipantsList',
-
-  setup() {
-    const state = reactive({
-      participants: '',
-    })
-
-    watch(
-      () => state.participants,
-      (participants, prevParticipants) => {
-        console.log('참석자 받았음', participants)
-        let participantsViews = document.getElementById('participants-views')
-        participantsViews.remove
-        participantsViews = document.createElement('div')
-        participantsViews.id = 'participants-views'
-
-        Object.getOwnPropertyNames(participants).forEach(
-          function (val, idx, array) {
-            createParticipant(participants[val])
-          }
-        )
-      }
-    )
-    const getParticipant = () => {
+  data: {
+    participants: ''
+  },
+  methods: {
+    getParticipant() {
       console.log('버튼 클릭')
-      state.participants = ''
-      state.participants = conference.getParticipants()
-    }
-
-    const createParticipant = (participant) => {
+      // state.participants = ''
+      this.participants = conference.getParticipants()
+    },
+    createParticipant(participant) {
       console.log("추가 되나요", participant, "참석자 이름", participant.name)
       const participantsViews = document.getElementById('participants-views')
 
@@ -55,9 +36,83 @@ export default {
       container.appendChild(nameTag)
       participantsViews.appendChild(container)
     }
+  },
+  watch: {
+    participants: function(newParti) {
+      console.log('참석자 받았음', this.participants)
+        let participantsViews = document.getElementById('participants-views')
+        participantsViews.remove
+        participantsViews = document.createElement('div')
+        participantsViews.id = 'participants-views'
 
-    return { state, watch, getParticipant, createParticipant }
-  }
+        Object.getOwnPropertyNames(this.participants).forEach(
+          function (val, idx, array) {
+            createParticipant(val)
+          }
+        )
+    }
+  },
+  // setup() {
+  //   const state = reactive({
+  //     participants: '',
+  //   })
+
+  //   watch(
+  //     () => state.participants,
+  //     (participants, prevParticipants) => {
+  //       console.log('참석자 받았음', participants)
+  //       let participantsViews = document.getElementById('participants-views')
+  //       participantsViews.remove
+  //       participantsViews = document.createElement('div')
+  //       participantsViews.id = 'participants-views'
+
+  //       Object.getOwnPropertyNames(participants).forEach(
+  //         function (val, idx, array) {
+  //           createParticipant(participants[val])
+  //         }
+  //       )
+  //     }
+  //     // (participants, prevParticipants) => {
+  //     //   console.log('참석자 받았음', participants)
+  //     //   let participantsViews = document.getElementById('participants-views')
+  //     //   participantsViews.remove
+  //     //   participantsViews = document.createElement('div')
+  //     //   participantsViews.id = 'participants-views'
+
+  //     //   Object.getOwnPropertyNames(participants).forEach(
+  //     //     function (val, idx, array) {
+  //     //       createParticipant(participants[val])
+  //     //     }
+  //     //   )
+  //     // }
+  //   )
+
+  //   const getParticipant = () => {
+  //     console.log('버튼 클릭')
+  //     // state.participants = ''
+  //     state.participants = conference.getParticipants()
+
+  //     createParticipant(conference.getParticipants())
+  //   }
+
+  //   const createParticipant = (participant) => {
+  //     console.log("추가 되나요", participant, "참석자 이름", participant.name)
+  //     const participantsViews = document.getElementById('participants-views')
+
+  //     const container = document.createElement('div')
+  //     container.id = 'aParticipant'
+  //     container.className = ''
+
+  //     const nameTag = document.createElement('div')
+  //     nameTag.id = 'name-tag'
+  //     nameTag.innerText = participant.name
+
+  //     container.appendChild(nameTag)
+  //     participantsViews.appendChild(container)
+  //   }
+
+  //   return { state, watch, getParticipant, createParticipant }
+  // }
 }
 </script>
 
