cbd505546300a0c4bb735933a77f50e3395d90ea
kangazy7 <hg_kang7@Naver.com>
2021-08-17 21:10:53 +0900

    [FE] fix: 화면,음성 onoff 기능 var localStream 추가

--- frontend/src/views/conferences/conference.vue
+++ frontend/src/views/conferences/conference.vue
@@ -173,25 +173,25 @@ export default {
 		},
 		
 		displayOff() {
-			localStream = navigator.mediaDevices.getUserMedia(this.constraints)
+			var localStream = await navigator.mediaDevices.getUserMedia(this.constraints)
 			console.log('pauseVideo', arguments);
 			localStream.getVideoTracks()[0].enabled = false;
     
   	},
 		displayOn() {
-			localStream = navigator.mediaDevices.getUserMedia(this.constraints)
+			var localStream = await navigator.mediaDevices.getUserMedia(this.constraints)
 			console.log('resumeVideo', arguments);
 			localStream.getVideoTracks()[0].enabled = true;
     
 		},
 		micOn() {
-			localStream = navigator.mediaDevices.getUserMedia(this.constraints)
+			var localStream = await navigator.mediaDevices.getUserMedia(this.constraints)
 			console.log('unmuteAudio', arguments);
 			localStream.getAudioTracks()[0].enabled = true;
     
   	},
 		micOff() {
-			localStream = navigator.mediaDevices.getUserMedia(this.constraints)
+			var localStream = await navigator.mediaDevices.getUserMedia(this.constraints)
 			console.log('muteAudio', arguments);
 			localStream.getAudioTracks()[0].enabled = false;
     
